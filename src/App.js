3675d5009d16e33d43c735a95a724cb539379ec6
summer <levantung14112002@gmail.com>
2023-04-16 23:41:53 +0700

    "comit code layout website"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+
+
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar"
+import RightBar from "./components/rightBar/RightBar"
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
+  Outlet,
 
 } from "react-router-dom";
 
@@ -11,12 +16,37 @@ import {
 
 function App() {
 
+  const Layout = () => {
+    return (
+      <div className="theme-light">
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
 
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <h1>Home page</h1>
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <h1>Home page</h1>
+        },
+        {
+          path: "/profile/:id",
+          element: <h1>Profile Page</h1>
+        }
+      ]
+
     },
     {
       path: "/login",
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import Friends from "../../assets/1.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+
+
+    return (
+        <div className="navbar">
+            <div className="left">
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <span>Summer Social</span>
+                </Link>
+                <HomeOutlinedIcon />
+
+                <DarkModeOutlinedIcon />
+                <GridViewOutlinedIcon />
+                <div className="search">
+                    <SearchOutlinedIcon />
+                    <input type="text" placeholder="Search..." />
+                </div>
+            </div>
+            <div className="right">
+                <PersonOutlinedIcon />
+                <EmailOutlinedIcon />
+                <NotificationsOutlinedIcon />
+                <div className="user">
+                    <img
+                        src={Friends}
+                        alt=""
+                    />
+                    <span>Summer</span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;

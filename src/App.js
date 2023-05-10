aa18f3ceb363053148561029aba7e84ea6faf704
summer <levantung14112002@gmail.com>
2023-05-10 23:20:19 +0700

    Tung push code auth, redux, context

--- src/App.js
+++ src/App.js
@@ -1,64 +1,107 @@
 
 
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Home from "./pages/home/Home";
-import Navbar from "./components/navbar/Navbar";
-import LeftBar from "./components/leftBar/LeftBar"
-import RightBar from "./components/rightBar/RightBar"
+
 import {
   createBrowserRouter,
-  RouterProvider,
+  Navigate,
   Outlet,
-
+  RouterProvider,
+  useLocation,
 } from "react-router-dom";
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Home from './pages/home/Home';
+import NavBar from './components/navbar/Navbar';
+import LeftBar from './components/leftBar/LeftBar';
+import RightBar from './components/rightBar/RightBar';
+import './style.scss';
+import { DarkModeContext } from "./context/darkModeContext";
+import { useContext, useEffect } from "react";
 
-
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
 
 function App() {
-
+  
+  
+  const currentUser = useSelector((state) => state.user.currentUser);
+  
+  
+  
+  const {darkMode} = useContext(DarkModeContext)
+  const queryClient = new QueryClient()
   const Layout = () => {
     return (
-      <div className="theme-light">
-        <Navbar />
-        <div style={{ display: "flex" }}>
-          <LeftBar />
-          <div style={{ flex: 6 }}>
-            <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <NavBar />
+          <div style={{display:"flex"}}>
+            <LeftBar />
+            <div style={{flex:6}}>
+              <Outlet />
+            </div>
+            <RightBar />
           </div>
-          <RightBar />
         </div>
-      </div>
-    );
-  };
+      </QueryClientProvider>
+    )
+  }
+  const HeaderLayout = ({children}) => {
+    return (
+        <div className={`theme-${darkMode ? "dark" : "light"}`}>
+          <NavBar />
+          <div>
+            {children}
+          </div>
+        </div>
+    )
+  }
 
+  const ProtectedRoute = ({children}) => {
+    const url = useLocation().pathname
+    useEffect(() => {
+      const scrollToTop = () =>{
+        window.scrollTo({
+          top: 0, 
+          behavior: 'smooth'
+        });
+      };
+      scrollToTop();
+    }, [url])
+    if(!currentUser){
+      return <Navigate to="/login" />
+    }
+    return children
+  }
+
+  const Logged = ({children}) => {
+    if(currentUser){
+      return <Navigate to="/" />
+    }
+    return children
+  }
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: <ProtectedRoute><Layout/></ProtectedRoute>,
       children: [
         {
-          path: "/",
+          path:"/",
           element: <Home />
-        },
-        {
-          path: "/profile/:id",
-          element: <h1>Profile Page</h1>
         }
+        
       ]
-
     },
     {
       path: "/login",
-      element: <Login />,
+      element: <Logged><Login /></Logged>,
     },
     {
       path: "/register",
-      element: <Register />,
+      element: <Logged><Register /></Logged>,
     },
   ]);
-
   return (
     <div>
       <RouterProvider router={router} />
